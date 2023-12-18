399a2a31c27ea4fa8d23ab8b96681dc256dcf2f2
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-12-18 20:13:56 +0600

    clean Up test console

--- app/api/rating/route.ts
+++ app/api/rating/route.ts
@@ -2,35 +2,38 @@ import getCurrentUser from "@/actions/getCurrentUser";
 import { Review } from "@prisma/client";
 import { NextResponse } from "next/server";
 
+export async function POST(request: Request) {
+  const currentUser = await getCurrentUser();
 
-export async function POST(request: Request){
-    const currentUser = await getCurrentUser();
+  if (!currentUser) {
+    return NextResponse.error();
+  }
 
-    if(!currentUser){
-        return NextResponse.error();
-    }
+  const body = await request.json();
+  const { comment, rating, product, userId } = body;
 
-    const body = await request.json()
-    const {comment, rating, product, userId} = body;
+  const deliveredOrder = currentUser?.orders.some(
+    (order) =>
+      order.products.find((item) => item.id === product.id) &&
+      order.deliveryStatus === "delivered"
+  );
 
-    const deliveredOrder = currentUser?.orders.some(order => order.products.find(item => item.id === product.id) && order.deliveryStatus === 'delivered')
+  const userReview = product?.reviews.find((review: Review) => {
+    return review.userId === currentUser.id;
+  });
 
-    const userReview = product?.reviews.find(((review: Review) =>{
-        return review.userId === currentUser.id
-    }))
+  if (userReview || !deliveredOrder) {
+    return NextResponse.error();
+  }
 
-    if(userReview || !deliveredOrder){
-        return NextResponse.error()
-    }
+  const review = await prisma?.review.create({
+    data: {
+      comment,
+      rating,
+      productId: product.id,
+      userId,
+    },
+  });
 
-    const review = await prisma?.review.create({
-        data: {
-            comment,
-            rating,
-            productId: product.id,
-            userId
-        }
-    })
-
-    return NextResponse.json(review)
+  return NextResponse.json(review);
 }
--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -12,12 +12,11 @@ interface ProductCardProps {
 
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
   const router = useRouter();
+  console.log("data", data);
 
   const productRating =
     data.reviews.reduce((acc: number, item: any) => item.rating + acc, 0) /
     data.reviews.length;
-
-  console.log(productRating);
 
   return (
     <div
