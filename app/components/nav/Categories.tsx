0ffbd140fecacad000b50771b43b6ed46829a3e6
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-12-18 21:46:34 +0600

    SearchBar and Categories is in one section

--- app/components/nav/Categories.tsx
+++ app/components/nav/Categories.tsx
@@ -1,32 +1,39 @@
-'use client'
+"use client";
 
 import { categories } from "@/utils/Categories";
 import Container from "../Container";
 import Category from "./Category";
 import { usePathname, useSearchParams } from "next/navigation";
+import SearchBar from "./SearchBar";
 
 const Categories = () => {
-    const params = useSearchParams()
-    const category = params?.get('category')
-    const pathname = usePathname();
-    const isMainPage = pathname === '/'
+  const params = useSearchParams();
+  const category = params?.get("category");
+  const pathname = usePathname();
+  const isMainPage = pathname === "/";
 
-    if(!isMainPage) return null;
+  if (!isMainPage) return null;
 
-    return (<div className="bg-white">
-        <Container>
-            <div className="pt-4 flex flex-row items-center justify-between overflow-x-auto">
-                {categories.map((item) =>(
-                    <Category
-                        key={item.label}
-                        label={item.label}
-                        icon={item.icon}
-                        selected= {category === item.label || (category === null && item.label === 'All')}
-                    />
-                ))}
-            </div>
-        </Container>
-    </div> );
-}
- 
+  return (
+    <div className="bg-white">
+      <Container>
+        <div className="pt-4 flex flex-row items-center justify-between overflow-x-auto">
+          {categories.map((item) => (
+            <Category
+              key={item.label}
+              label={item.label}
+              icon={item.icon}
+              selected={
+                category === item.label ||
+                (category === null && item.label === "All")
+              }
+            />
+          ))}
+          <SearchBar />
+        </div>
+      </Container>
+    </div>
+  );
+};
+
 export default Categories;
