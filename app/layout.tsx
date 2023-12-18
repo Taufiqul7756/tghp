1c6c714425a175301bfeb1468d1b05d91800fc36
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-12-18 15:06:42 +0600

    title and des

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ import { Toaster } from "react-hot-toast";
 const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
 
 export const metadata: Metadata = {
-  title: "E-Shop",
-  description: "Ecommerce app",
+  title: "TGHP",
+  description: "Blog and Shop",
 };
 
 export default async function RootLayout({
