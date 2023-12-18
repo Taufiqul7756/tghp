b12c3f88397d399bd217e524c9a27ac27ac8f8ed
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-12-18 20:45:33 +0600

    Categories section removed

--- app/components/nav/Category.tsx
+++ app/components/nav/Category.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { useSearchParams } from "next/dist/client/components/navigation";
 import { useRouter } from "next/navigation";
@@ -6,51 +6,60 @@ import queryString from "query-string";
 import { useCallback } from "react";
 import { IconType } from "react-icons";
 
-interface CategoryProps{
-    label: string;
-    icon: IconType;
-    selected?: boolean
+interface CategoryProps {
+  label: string;
+  icon: IconType;
+  selected?: boolean;
 }
 
-const Category:React.FC<CategoryProps> = ({label, icon:Icon, selected}) => {
-    const router = useRouter()
-    const params = useSearchParams()
+const Category: React.FC<CategoryProps> = ({ label, icon: Icon, selected }) => {
+  const router = useRouter();
+  const params = useSearchParams();
 
-    const handleClick = useCallback(() => {
-        if(label === 'All'){
-            router.push('/')
-        }else{
-            let currentQuery = {};
+  const handleClick = useCallback(() => {
+    if (label === "All") {
+      router.push("/");
+    } else {
+      let currentQuery = {};
 
-            if(params){
-                currentQuery = queryString.parse(params.toString())
-            }
+      if (params) {
+        currentQuery = queryString.parse(params.toString());
+      }
 
-            const updatedQuery:any = {
-                ...currentQuery,
-                category: label
-            }
+      const updatedQuery: any = {
+        ...currentQuery,
+        category: label,
+      };
 
-            const url = queryString.stringifyUrl(
-                {
-                    url: '/',
-                    query: updatedQuery
-                },
-                {
-                    skipNull: true
-                }
-            )
-
-            router.push(url)
+      const url = queryString.stringifyUrl(
+        {
+          url: "/",
+          query: updatedQuery,
+        },
+        {
+          skipNull: true,
         }
-    }, [label, params, router])
+      );
 
-    return ( <div onClick={handleClick} className={`flex items-center justify-center text-center gap-1 p-2 border-b-2 hover:text-slate-800 transition cursor-pointer
-    ${selected ? 'border-b-slate-800 text-slate-800' : 'border-transparent text-slate-500'}
-    `}>
-        <Icon size={20}/>
-        <div className="font-medium text-sm">{label}</div>
-    </div> );
-}
- 
+      router.push(url);
+    }
+  }, [label, params, router]);
+
+  return (
+    <div
+      onClick={handleClick}
+      className={`flex items-center justify-center text-center gap-1 p-2 border-b-2 hover:text-slate-800 transition cursor-pointer
+    ${
+      selected
+        ? "border-b-slate-800 text-slate-800"
+        : "border-transparent text-slate-500"
+    }
+    `}
+    >
+      <Icon size={20} />
+      <div className="font-medium text-sm">{label}</div>
+    </div>
+  );
+};
+
 export default Category;
--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -50,7 +50,7 @@ const NavBar = async () => {
           </div>
         </Container>
       </div>
-      <Categories />
+      {/* <Categories /> */}
     </div>
   );
 };
